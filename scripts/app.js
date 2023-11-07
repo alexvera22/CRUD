db383bd1bc168326212228b24c89a3a0c357a3d9
alexvera22 <alex.uru.vb@gmail.com>
2023-11-07 17:53:35 -0300

    Función al botón Modificar y al modal para que guarden los datos

--- scripts/app.js
+++ scripts/app.js
@@ -92,3 +92,75 @@
     document.getElementById('btnPost').addEventListener('click', () =>{
      agregarRegistro();
     });
+
+// Evento para el botón "Modificar" para que funcione al darle click
+document.getElementById('btnPut').addEventListener('click', () => {
+    // Obtenemos el ID del registro a modificar
+    const id = document.getElementById('inputPutId').value;
+
+    // Realiza una solicitud a la API Mock para obtener los datos del registro
+    fetch(`https://65482f3add8ebcd4ab229fd7.mockapi.io/users/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.id) {
+                // Rellenamos los campos del modal con los valores del registro
+                document.getElementById('inputPutNombre').value = data.name;
+                document.getElementById('inputPutApellido').value = data.lastname;
+
+                // Habilitamos el botón "Guardar Cambios" para que queda activo para guardar
+                document.getElementById('btnSendChanges').removeAttribute('disabled');
+
+                // Muestra el modal de Bootstrap que ya esta incluido en el html
+                const dataModal = new bootstrap.Modal(document.getElementById('dataModal'));
+                dataModal.show();
+            } else {
+                mostrarError('Registro no encontrado.');
+            }
+        })
+        .catch(error => {
+            mostrarError('Ocurrió un error al obtener el registro.');
+        });
+});
+
+// Event listener para habilitar/deshabilitar el botón "Modificar" cuando se ingresa un ID esto va a hacer que el boton modificar quede habilitado
+document.getElementById('inputPutId').addEventListener('input', habilitarBotonModificar);
+
+function habilitarBotonModificar() {
+    const id = document.getElementById('inputPutId').value;
+    const btnModificar = document.getElementById('btnPut');
+    btnModificar.disabled = id === '';
+}
+// Llama a la función para habilitar o deshabilitar el botón "Modificar" al cargar la página
+habilitarBotonModificar();
+
+//Acá hacemos un event listener para el botón guardar que al darle click envíe los datos actualizados a la api
+document.getElementById('btnSendChanges').addEventListener('click', () => {
+    // Obtiene el ID del registro a modificar
+    const id = document.getElementById('inputPutId').value;
+
+    // Obtiene los nuevos valores de "Nombre" y "Apellido" del modal
+    const nuevoNombre = document.getElementById('inputPutNombre').value;
+    const nuevoApellido = document.getElementById('inputPutApellido').value;
+
+    // Realiza la solicitud PUT para actualizar el registro
+    fetch(`https://65482f3add8ebcd4ab229fd7.mockapi.io/users/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: nuevoNombre, lastname: nuevoApellido })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Actualiza la lista de registros
+        listarRegistros();
+
+        // Cierra el modal para que se pueda seguir interactuando con la pagina
+        const modalElement = document.getElementById('dataModal');
+        const modalInstance = bootstrap.Modal.getInstance(modalElement);
+        modalInstance.hide();
+    })
+    .catch(error => {
+        mostrarError('Ocurrió un error al guardar los cambios.');
+    });
+});
