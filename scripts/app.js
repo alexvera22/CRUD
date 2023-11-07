46ea29fa1a1b1eeef2c6a2e5e0f9416bfb81c810
alexvera22 <alex.uru.vb@gmail.com>
2023-11-06 21:35:33 -0300

    Funcion al botón agregar

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 
-    // Función para hacer una solicitud GET a la API Mock para listar registros.
+    // Función para hacer una solicitud GET a la API Mock para listar registros
     function listarRegistros() {
         let id = document.getElementById('inputGet1Id').value;
 
@@ -21,7 +21,7 @@
             });
     }
 
-    // Función para mostrar una lista de registros en el elemento con ID "results".
+    // Función para mostrar una lista de registros en el elemento con ID "results"
     function mostrarResultados(registros) {
         let resultsElement = document.getElementById('results');
         resultsElement.innerHTML = '';
@@ -33,20 +33,62 @@
         });
     }
 
-    // Función para mostrar un solo registro en el elemento con ID "results".
+    // Función para mostrar un solo registro en el elemento con ID "results"
     function mostrarResultado(registro) {
         let resultsElement = document.getElementById('results');
         resultsElement.innerHTML = `ID: ${registro.id}, Nombre: ${registro.name}, Apellido: ${registro.lastname}`;
     }
-
-    // Función para mostrar un mensaje de error en el elemento con ID "alert-error".
+    function agregarRegistro() {
+        const nombre = document.getElementById('inputPostNombre').value;
+        const apellido = document.getElementById('inputPostApellido').value;
+    
+        if (nombre && apellido) {
+            const nuevoRegistro = {
+                name: nombre,
+                lastname: apellido
+            };
+    
+            fetch('https://65482f3add8ebcd4ab229fd7.mockapi.io/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(nuevoRegistro)
+            })
+            .then(response => response.json())
+            .then(data => {
+                listarRegistros(); // Actualiza la lista después de agregar un registro
+            })
+            .catch(error => {
+                mostrarError('Ocurrió un error al agregar el registro.');
+            });
+        } else {
+            mostrarError('Por favor, complete los campos de nombre y apellido.');
+        }
+    }
+    function habilitarBotonAgregar() {
+        const nombre = document.getElementById('inputPostNombre').value;
+        const apellido = document.getElementById('inputPostApellido').value;
+        const btnPost = document.getElementById('btnPost');
+        btnPost.disabled = !(nombre && apellido);
+    }
+    // Función para mostrar un mensaje de error en el elemento con ID "alert-error"
     function mostrarError(mensaje) {
         let alertError = document.getElementById('alert-error');
         alertError.textContent = mensaje;
         alertError.classList.remove('fade');
     }
 
-    // Event listener para el botón "Buscar".
+    // Evento para el botón "Buscar"
     document.getElementById('btnGet1').addEventListener('click', () => {
         listarRegistros();
+    });
+
+    // Event listener para habilitar/deshabilitar el botón "Agregar" cuando se ingresan datos
+    document.getElementById('inputPostNombre').addEventListener('input', habilitarBotonAgregar);
+    document.getElementById('inputPostApellido').addEventListener('input', habilitarBotonAgregar);
+
+    //Evento para el botón "Agregar"
+    document.getElementById('btnPost').addEventListener('click', () =>{
+     agregarRegistro();
     });
